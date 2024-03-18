58f475c3bc35dc73ac820125710ddb821f27013b
João Paulo Dagostin <dagostinjota@gmail.com>
2024-03-18 11:59:42 +0100

    ts update code
    new decorator

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspect.js";
 import { logTimeExecution } from "../decorators/loggin-time-execution.js";
 
 export abstract class View<T> {
@@ -18,6 +19,7 @@ export abstract class View<T> {
     }
   }
 
+  @inspect()
   @logTimeExecution(true)
   public update(model: T): void {
     let template = this.template(model);
